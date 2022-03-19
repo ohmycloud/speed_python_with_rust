560f3856b8a260af9215015c2ca84300e0ae0670
ohmycloud <ohmycloudy@gmail.com>
2022-03-19 22:09:56 +0800

    add tests

--- src/fib_calcs/fib_numbers.rs
+++ src/fib_calcs/fib_numbers.rs
@@ -1,12 +1,23 @@
-use std::vec::Vec;
-use pyo3::prelude::pyfunction;
 use super::fib_number::fibonacci_number;
+use pyo3::prelude::pyfunction;
+use std::vec::Vec;
 
 #[pyfunction]
 pub fn fibonacci_numbers(numbers: Vec<i32>) -> Vec<u64> {
-  let mut vec: Vec<u64> = Vec::new();
-  for n in numbers.iter() {
-    vec.push(fibonacci_number(*n));
-  }
-  vec
+    let mut vec: Vec<u64> = Vec::new();
+    for n in numbers.iter() {
+        vec.push(fibonacci_number(*n));
+    }
+    vec
+}
+
+#[cfg(test)]
+mod fibonacci_numbers_tests {
+    use super::fibonacci_numbers;
+
+    #[test]
+    fn test_run() {
+        let outcome = fibonacci_numbers([1, 2, 3, 4].to_vec());
+        assert_eq!(outcome, [1, 1, 2, 3]);
+    }
 }
