bbbb1762c9c9a01faebb798cbbdb5c269c126d0f
ohmycloud <ohmycloudy@gmail.com>
2022-03-20 14:34:57 +0800

    constructing_our_own_python_objects

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ mod interface;
 use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
 use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
 use interface::config::__pyo3_get_function_run_config;
+use interface::object::__pyo3_get_function_object_interface;
 
 #[pyfunction]
 fn say_hello() {
@@ -20,5 +21,6 @@ fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(fibonacci_number));
     m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
     m.add_wrapped(wrap_pyfunction!(run_config));
+    m.add_wrapped(wrap_pyfunction!(object_interface));
     Ok(())
 }
