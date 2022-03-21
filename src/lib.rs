cc4b38a27a48292a9ce9b59a261a47103a14194e
ohmycloud <ohmycloudy@gmail.com>
2022-03-21 14:18:33 +0800

    using numpy in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
+use pyo3::types::PyDict;
 
 mod fib_calcs;
 
@@ -12,6 +13,27 @@ use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
 use interface::config::__pyo3_get_function_run_config;
 use interface::object::__pyo3_get_function_object_interface;
 use class_module::fib_processor::FibProcessor;
+
+#[pyfunction]
+fn test_numpy<'a>(result_dict: &'a PyDict) -> PyResult<&'a PyDict> {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let locals = PyDict::new(py);
+    locals.set_item("np", py.import("numpy").unwrap());
+
+    let code = "np.array[[3,2], [1, 4]]";
+    let weights_matrix = py.eval(code, None, Some(&locals)).unwrap();
+    locals.set_item("weights_matrix", weights_matrix);
+
+    let new_code = "np.array([[10], [20]])";
+    let input_matrix = py.eval(new_code, None, Some(&locals)).unwrap();
+    locals.set_item("input_matrix",  input_matrix);
+
+    let calc_code = "np.dot(weights_matrix, input_matrix)";
+    let result_end = py.eval(calc_code, None, Some(&locals)).unwrap();
+    result_dict.set_item("numpy result", result_end);
+    Ok(result_dict)
+}
 
 #[pyfunction]
 fn time_add_vectors(total_vector_size: i32) -> Vec<i32> {
@@ -37,6 +59,7 @@ fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(run_config));
     m.add_wrapped(wrap_pyfunction!(object_interface));
     m.add_wrapped(wrap_pyfunction!(time_add_vectors));
+    m.add_wrapped(wrap_pyfunction!(test_numpy));
     m.add_class::<FibProcessor>()?;
     Ok(())
 }
