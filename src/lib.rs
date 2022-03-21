d4a5d743f6a05219403249f8318f7a9cba3cbabd
sunxiaowei(孙小伟) <sunxiaowei@thinkenergy.net>
2022-03-21 10:53:25 +0800

    add two vector

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,17 @@ use interface::object::__pyo3_get_function_object_interface;
 use class_module::fib_processor::FibProcessor;
 
 #[pyfunction]
+fn time_add_vectors(total_vector_size: i32) -> Vec<i32> {
+  let mut buffer: Vec<i32> = Vec::new();
+  let first_vector: Vec<i32> = (0..total_vector_size).map( |x| x).collect();
+  let second_vector: Vec<i32> = (0..total_vector_size).map(|x| x).collect();
+  for i in &first_vector {
+      buffer.push(first_vector[*i as usize] + second_vector[*i as usize]);
+  }
+  buffer
+}
+
+#[pyfunction]
 fn say_hello() {
     println!("saying hello from Rust!");
 }
@@ -25,6 +36,7 @@ fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
     m.add_wrapped(wrap_pyfunction!(run_config));
     m.add_wrapped(wrap_pyfunction!(object_interface));
+    m.add_wrapped(wrap_pyfunction!(time_add_vectors));
     m.add_class::<FibProcessor>()?;
     Ok(())
 }
